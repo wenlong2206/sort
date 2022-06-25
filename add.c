a60ffc31905e5c06ed19eace0af44a9845140c8b
wenlong2206 <108123564+wenlong2206@users.noreply.github.com>
2022-06-25 15:28:36 +0800

    增加一个数字
    使用C语言进行整数加法求和计算

--- add.c
+++ add.c
@@ -2,18 +2,19 @@
 #include <stdio.h>
 #include <assert.h>
 
-int add(int a, int b)
+int add(int a, int b, int c)
 {
-  return a + b;
+  return a + b + c;
 }
 
 int mian()
 {
   int a = 10;
   int b = 20;
+  int c = 30
   
-  int c = add(a, b)
-  printf("%d", c)
+  int num = add(a, b, c)
+  printf("%d", num)
   
   return 0;
 }
