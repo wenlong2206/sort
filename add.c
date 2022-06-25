7dd6309858b676d93b681cf062bbe93c36b5486b
wenlong2206 <108123564+wenlong2206@users.noreply.github.com>
2022-06-25 15:19:28 +0800

    创建一个C文件的测试代码

---
+++ add.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+int add(int a, int b)
+{
+  return a + b;
+}
+
+int mian()
+{
+  int a = 10;
+  int b = 20;
+  
+  int c = add(a, b)
+  printf("%d", c)
+  
+  return 0;
+}
