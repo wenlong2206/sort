579d62200f95eb5a383b75b35fdd53367d8be770
wenlong2206 <108123564+wenlong2206@users.noreply.github.com>
2022-06-25 15:36:45 +0800

    添加一个C语言减法运算函数
    在文件sub.c中使用C语言进行减法运算

---
+++ sub.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#inlcude <assert.h>
+#include <stdio.h>
+
+
+int sub(int a, int b)
+{
+  return a - b;
+}
+
+
+int main(void)
+{
+  int a = 10;
+  int b = 20;
+  
+  int num = 0;
+  num = sub(a, b)
+  
+  return num;
+}
